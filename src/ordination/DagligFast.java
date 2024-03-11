e68ea5f31bfe550d6cb9ca7e4a609ebd87140c1d
Michaela <michaela@loenquist.dk>
2024-03-11 10:54:32 +0100

    DagligFast og DagligSkaev

--- src/ordination/DagligFast.java
+++ src/ordination/DagligFast.java
@@ -1,5 +1,32 @@
 package ordination;
 
-public class DagligFast {
-    // TODO
+import java.time.LocalDate;
+
+public class DagligFast extends Ordination{
+    // TODO Kompositionen mellem DagligFast og Dosis skal realiseres ved et array af doser: Dosis[4]
+    private Dosis[] doser = new Dosis[4];
+
+    public DagligFast(LocalDate startDen, LocalDate slutDen, Laegemiddel laegemiddel, Dosis[] doser) {
+        super(startDen, slutDen, laegemiddel);
+        this.doser = doser;
+    }
+
+    public Dosis[] getDoser() {
+        return doser;
+    }
+
+    @Override
+    public double samletDosis() {
+        return 0;
+    }
+
+    @Override
+    public double doegnDosis() {
+        return 0;
+    }
+
+    @Override
+    public String getType() {
+        return null;
+    }
 }
--- src/ordination/DagligSkaev.java
+++ src/ordination/DagligSkaev.java
@@ -1,12 +1,39 @@
 package ordination;
 
+import java.time.LocalDate;
 import java.time.LocalTime;
+import java.util.ArrayList;
 
-public class DagligSkaev {
-    // TODO
+public class DagligSkaev extends Ordination{
+    // TODO Kompositionen mellem DagligSkæv og Dosis skal realiseres ved en ArrayList<Dosis>
+    private ArrayList<Dosis> doser = new ArrayList<>();
 
+    public DagligSkaev(LocalDate startDen, LocalDate slutDen, Laegemiddel laegemiddel, ArrayList<Dosis> doser) {
+        super(startDen, slutDen, laegemiddel);
+        this.doser = doser;
+    }
+
+    public ArrayList<Dosis> getDoser() {
+        return doser;
+    }
 
     public void opretDosis(LocalTime tid, double antal) {
         // TODO
+        Dosis dosis = new Dosis(tid, antal);
+    }
+
+    @Override
+    public double samletDosis() {
+        return 0;
+    }
+
+    @Override
+    public double doegnDosis() {
+        return 0;
+    }
+
+    @Override
+    public String getType() {
+        return null;
     }
 }
