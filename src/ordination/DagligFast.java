609fa1277861a81b3fca64903fb41dfde8628ef1
Michaela <michaela@loenquist.dk>
2024-03-11 22:01:05 +0100

    For loop doegnDosis

--- src/ordination/DagligFast.java
+++ src/ordination/DagligFast.java
@@ -2,7 +2,7 @@ package ordination;
 
 import java.time.LocalDate;
 
-public class DagligFast extends Ordination{
+public class DagligFast extends Ordination {
     // TODO Kompositionen mellem DagligFast og Dosis skal realiseres ved et array af doser: Dosis[4]
     private Dosis[] doser = new Dosis[4];
 
@@ -24,8 +24,18 @@ public class DagligFast extends Ordination{
     // Vi skal have fundet måden af finde ud af om patienten er let/normal/tung. Jeg har kun normal metode
     @Override
     public double doegnDosis() {
-        double samlet = getLaegemiddel().getEnhedPrKgPrDoegnNormal() * 4;
-        return samlet;
+        double doegnDosis = 0;
+        for (int i = 0; i < doser.length; i++) {
+            if (doser != null) {
+                doegnDosis += doser[i].getAntal();
+            }
+        }
+        return doegnDosis;
+        // Jeg har lavet et for loop på doegndosis, og udkommenteret nedenstående, i tilfælde af, at jeg er helt galt på den med for loop.
+        // Årsagen er, at man med loop først tjekker om der er dosis på alle fire tidspunkter, og dermed kun medtager de tidspunkter der har en ordination
+        // mens nedenstående ikke tager højde for dette.
+        // double samlet = getLaegemiddel().getEnhedPrKgPrDoegnNormal() * 4;
+        // return samlet;
     }
 
     @Override
