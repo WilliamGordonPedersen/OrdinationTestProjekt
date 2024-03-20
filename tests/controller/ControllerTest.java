457e8d47ac3cd551803b5ecf2151b6dbdde6a53f
DanielKongen <Danielkonge1@Gmail.com>
2024-03-20 12:10:20 +0100

    Test dægnDosis daglig Fast

--- tests/controller/ControllerTest.java
+++ tests/controller/ControllerTest.java
@@ -18,26 +18,15 @@ class ControllerTest {
     void opretDagligSkaevOrdination() {
 
         Controller controller1 = Controller.getController();
+        controller1.createSomeObjects();
         Patient p = new Patient("12", "Palle", 60);
         Laegemiddel laegemiddel = new Laegemiddel("paracetamol", 5.0, 5.0, 5.0, "ml");
         LocalTime[] klokkeslet = new LocalTime[10];
         klokkeslet[0]=LocalTime.of(10, 30);
-
         double[]antalEnheder = new double[10];
+
         antalEnheder[0]=5;
-
-
-
-
-        DagligSkaev actual = controller1.opretDagligSkaevOrdination(LocalDate.of(2024, 2, 6), LocalDate.of(2024, 2, 4), p, laegemiddel, klokkeslet, antalEnheder);
-        assertEquals(new IllegalArgumentException(), actual);
-
-
-
-
-
-
-
-
+        DagligSkaev actual = controller1.opretDagligSkaevOrdination(LocalDate.of(2024, 2, 3), LocalDate.of(2024, 2, 4), p, laegemiddel, klokkeslet, antalEnheder);
+        assertNotNull(actual);
     }
 }
